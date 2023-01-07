44290f5eac91be8fc6a8d9925d50366a9aa97f63
Van Truong <vantruongtran1098@gmail.com>
2023-01-07 12:51:14 +0700

    12:51 07/01/2023

---
+++ src/main/java/filter/AuthorizationFilter.java
@@ -0,0 +1,33 @@
+package filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "AuthorizationFilter", urlPatterns = {"/*"})
+public class AuthorizationFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+
+        String servletPath = req.getServletPath();
+        String url = req.getRequestURI();
+
+
+
+
+
+
+
+
+
+        filterChain.doFilter(servletRequest, servletResponse);
+
+        //NOT PERMISSION
+
+
+    }
+}
